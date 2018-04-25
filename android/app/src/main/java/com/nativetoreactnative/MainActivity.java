6005e145e9f836fa95c969fcc0dcf499e5c42856
Sanket Bhaud <sanket.b@peopleinteractive.in>
2018-04-25 12:21:07 +0530

    Completed navigation from native to react native

---
+++ android/app/src/main/java/com/nativetoreactnative/MainActivity.java
@@ -0,0 +1,24 @@
+package com.nativetoreactnative;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_react);
+
+        findViewById(R.id.btn_show_react_page).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MyReactActivity.class));
+            }
+        });
+    }
+}
